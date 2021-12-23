8e3b544aa45aeaa86147b97c3977022f57cccd6a
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2021-12-23 17:55:58 +0100

    day 6 part 2 done thx to reddit and max

--- day6/6.py
+++ day6/6.py
@@ -3,13 +3,26 @@ content_list = open("day6.txt", "r").read()
 # part 1 and 2
 fish_list = [int(fish) for fish in content_list if fish.isdigit()]
 
-i = 0
-while i < 256:
-    for fish in range(len(fish_list)):
-        if fish_list[fish] == 0:
-            fish_list[fish] = 6
-            fish_list.append(8)
-        else:
-            fish_list[fish] -= 1
-    i += 1
-    print(i)
+fish_inv = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+for key in fish_inv:
+    fish_inv[key] = fish_list.count(key)
+
+for day in range(256):
+    day_inv = fish_inv.copy()
+
+    for key in day_inv:
+        if key != 0:
+            fish_inv[key-1] = day_inv[key]
+
+    for fish in day_inv:
+        if fish == 0:
+            fish_inv[8] = day_inv[fish]
+            fish_inv[6] += day_inv[fish]
+    print(day)
+
+print(day_inv)
+
+sum = 0
+for key in fish_inv:
+    sum += fish_inv[key]
+print(sum)
